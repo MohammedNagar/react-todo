b9cec40963e73f62eb10245403da064468bb7e3c
Anyname Donotcare <Anyname Donotcare>
2017-08-12 16:47:57 +0200

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -3,9 +3,17 @@ var ReactDOM = require('react-dom');
 var { BrowserRouter, Route } =require('react-router-dom');
 
 var TodoApp = require('TodoApp');
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log('CurrentState: ',store.getState());
+});
+
+store.dispatch(actions.addTodo('Play a game...'));
+store.dispatch(actions.setSearchText('game'));
+store.dispatch(actions.toggleShowCompleted());
 //Load foundation
-
-
 $(document).foundation();
 
 //app style
--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,8 @@ module.exports= {
        'app/components',
         'app/api',
         'app/actions',
-         'app/reducers'],
+         'app/reducers',
+       'app/store'],
     alias:{
       applicationStyles:'app/styles/app.scss'
 
